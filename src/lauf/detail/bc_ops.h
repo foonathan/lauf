e0eecf3dd5ae1863d49d29620b19d11f83380635
Jonathan Müller <git@foonathan.net>
2022-05-23 22:15:51 +0200

    Check for stack overflow in tail calls

--- src/lauf/detail/bc_ops.h
+++ src/lauf/detail/bc_ops.h
@@ -60,20 +60,35 @@ LAUF_BC_OP(call, bc_inst_function_idx, {
     LAUF_DISPATCH;
 })
 
+LAUF_BC_OP(tail_call, bc_inst_function_idx, {
+    auto callee = vm->get_function(ip->call.function_idx);
+
+    // We only need to allocate memory for the local variables, but not create a new stack frame.
+    // We don't need to worry about alignment here either.
+    if (!vm->memory_stack.allocate(callee->local_stack_size))
+    {
+        auto info = make_panic_info(frame_ptr, ip);
+        vm->panic_handler(&info, "stack overflow");
+        return false;
+    }
+
+    auto remaining_vstack_size = vstack_ptr - vm->value_stack_limit();
+    if (remaining_vstack_size < callee->max_vstack_size)
+    {
+        auto info = make_panic_info(frame_ptr, ip);
+        vm->panic_handler(&info, "value stack overflow");
+        return false;
+    }
+
+    ip = callee->bytecode();
+    LAUF_DISPATCH;
+})
+
 LAUF_BC_OP(call_builtin, bc_inst_offset_literal_idx, {
     auto callee = (lauf_builtin_function*)(vm->get_literal(ip->call_builtin.literal_idx).as_ptr);
     auto stack_change = ip->call_builtin.offset;
     ++ip;
     LAUF_DISPATCH_BUILTIN(callee, stack_change);
-})
-
-LAUF_BC_OP(tail_call, bc_inst_function_idx, {
-    auto callee = vm->get_function(ip->call.function_idx);
-    // We only need to allocate memory for the local variables, but not create a new stack frame.
-    // We don't need to worry about alignment here either.
-    vm->memory_stack.allocate(callee->local_stack_size);
-    ip = callee->bytecode();
-    LAUF_DISPATCH;
 })
 
 //=== literals ===//
