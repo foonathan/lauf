6a2eecbdd2ca8bd7eb4d7601b80f6795378dab63
Jonathan Müller <git@foonathan.net>
2022-10-23 16:35:21 +0200

    Use correct helper function for rounding in `lauf::page_allocator`
    It is also faster since it avoids a division.

--- src/lauf/support/page_allocator.cpp
+++ src/lauf/support/page_allocator.cpp
@@ -48,8 +48,7 @@ const auto real_page_size = [] {
 
 lauf::page_block lauf::page_allocator::allocate(std::size_t size)
 {
-    if (auto remainder = size % real_page_size)
-        size += real_page_size - remainder;
+    size = round_to_multiple_of_alignment(size, real_page_size);
 
     // Find a page_count pages in the free list.
     free_list_node* prev = nullptr;
@@ -84,8 +83,7 @@ std::size_t lauf::page_allocator::try_extend(page_block block, std::size_t new_s
     // * when deallocating we merge everything to keep it contiguous
     // So the only way to extend it is to ask the OS.
 
-    if (auto remainder = new_size % real_page_size)
-        new_size += real_page_size - remainder;
+    new_size = round_to_multiple_of_alignment(new_size, real_page_size);
 
     auto ptr = ::mremap(block.ptr, block.size, new_size, 0);
     if (ptr == MAP_FAILED) // NOLINT: macro
@@ -105,8 +103,7 @@ std::size_t lauf::page_allocator::try_extend(page_block block, std::size_t new_s
 
 void lauf::page_allocator::deallocate(page_block block)
 {
-    if (auto remainder = block.size % real_page_size)
-        block.size += real_page_size - remainder;
+    block.size = round_to_multiple_of_alignment(block.size, real_page_size);
 
     // Merge with an existing page block if possible.
     for (auto cur = _free_list; cur != nullptr; cur = cur->next)
